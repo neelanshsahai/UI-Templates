7646ead57b3003cb6b4e51ca7349c8f4cdc24d77
neelansh <96shubh@gmail.com>
2017-10-26 08:09:51 +0530

    "Add Material Tabs"

--- app/src/main/java/templates/ui/com/testui/MainActivity.java
+++ app/src/main/java/templates/ui/com/testui/MainActivity.java
@@ -1,83 +1,91 @@
 package templates.ui.com.testui;
 
-import android.support.design.widget.CollapsingToolbarLayout;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity {
-
-    private final List<Fragment> mFragmentList = new ArrayList<>();
-    private final List<String> mFragmentTitleList = new ArrayList<>();
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+            Tab1.OnFragmentInteractionListener,
+            Tab2.OnFragmentInteractionListener,
+            Tab3.OnFragmentInteractionListener {
 
     private Toolbar mToolbar;
     private DrawerLayout mDrawerLayout;
-    private TabLayout tabLayout;
-    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
-
-        viewPager = (ViewPager) findViewById(R.id.tabViewPager);
-        setupViewPager(viewPager);
-
-        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
-        tabLayout.setupWithViewPager(viewPager);
-
-        setupToolbarMenu();
-        setupNavigationDrawerMenu();
-
-    }
-
-    private void setupNavigationDrawerMenu() {
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        ActionBarDrawerToggle abdt = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open, R.string.close);
-        mDrawerLayout.addDrawerListener(abdt);
-        abdt.syncState();
-    }
-
-    private void setupToolbarMenu() {
+        //setupToolBar();
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
+        mToolbar.setTitle("Nav View Bro");
+
+        //setupNavigatioDrawerMenu();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Material Highlights"));
+        tabLayout.addTab(tabLayout.newTab().setText("Android Patterns"));
+        tabLayout.addTab(tabLayout.newTab().setText("Bonus"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
     }
 
-    private void setupViewPager(ViewPager viewPager) {
-        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
- //       adapter.addFragment(new OneFragment(), "MATERIAL HIGHLIGHTS");
- //       adapter.addFragment(new TwoFragment(), "ANDROID PATTERNS");
- //       adapter.addFragment(new ThreeFragment(), "BONUS");
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        mDrawerLayout.closeDrawer(GravityCompat.START);
+        return false;
     }
 
-    private class ViewPagerAdapter extends FragmentPagerAdapter {
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START))
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
 
-        public ViewPagerAdapter(FragmentManager fm) {super(fm);}
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
-        @Override
-        public Fragment getItem(int position) {return mFragmentList.get(position);}
-
-        @Override
-        public int getCount() {return mFragmentList.size();}
-
-        public void addFragment(Fragment f, String s) {
-            mFragmentList.add(f);
-            mFragmentTitleList.add(s);
-        }
     }
 }
